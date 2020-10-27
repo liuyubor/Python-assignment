f685f3633cb8ba78023b2b5b9ddd192dd28b681a
liuyubor <liuyubor@outlook.com>
2020-10-27 12:21:32 +0800

    fix bug

--- 实验课7/进制转换程序.py
+++ 实验课7/进制转换程序.py
@@ -1,13 +1,17 @@
 
-x = int(input("请输入待转换的整数："))
-r = int(input("请输入转换的进制数（r>=2）："))
+x = input("请输入待转换的整数：")
+r = input("请输入转换的进制数（r>=2）：")
 result = ''
-while x != 0:
-    num = x % r
-    x //= r
-    if num > 9:
-        result += chr(num+55)
-    else:
-        result += str(num)
-
-print(result[::-1])
+if x or r == "q":
+    pass
+else:
+    while x != 0:
+        x=int(x)
+        r=int(r)
+        num = x % r
+        x //= r
+        if num > 9:
+            result += chr(num+55)
+        else:
+            result += str(num)
+    print(result[::-1])
