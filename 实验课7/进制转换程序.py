b91b14953483818477b7a063069cda8e145e8043
liuyubor <liuyubor@outlook.com>
2020-10-27 12:11:42 +0800

    实验七

--- 实验课7/进制转换程序.py
+++ 实验课7/进制转换程序.py
@@ -1,3 +1,13 @@
 
-num = int(input("请输入待转换的整数："))
+x = int(input("请输入待转换的整数："))
 r = int(input("请输入转换的进制数（r>=2）："))
+result = ''
+while x != 0:
+    num = x % r
+    x //= r
+    if num > 9:
+        result += chr(num+55)
+    else:
+        result += str(num)
+
+print(result[::-1])
